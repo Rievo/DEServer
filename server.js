aed7181541e2035f6d9d6bdfd857e744d774bbe2
Diego <diego@akenaton.ii.uam.es>
2016-01-18 14:36:36 +0100

    some changes

--- server.js
+++ server.js
@@ -429,7 +429,7 @@ app.use(router);
 console.log("name: " + user);
 console.log("pass: " + pass);
 
-mongoose.connect("mongodb://"+user+":"+pass +"@ds047355.mongolab.com:47355/diagrameditor");
+mongoose.connect("mongodb://"+user+":"+pass +"@ds047355.mongolab.com:47355/diagrameditor",{auth:{authdb:"admin"}});
 
 //Start listening
 app.listen(port, function() {  
