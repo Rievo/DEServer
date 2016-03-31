586fed64ef121d085bbc11d7463c1062e2b8651a
Diego <diego@akenaton.ii.uam.es>
2016-03-31 15:03:46 +0200

    aa

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ var childProcess = require('child_process');
 var mkdirp = require('mkdirp');
 
 
-var path = require('path');
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(bodyParser.json());  
+var path = require('path'); 
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(methodOverride());
 
 app.set('views', __dirname + '/views')
